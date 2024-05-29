419c58bc0750ea21ef4c3c59b7433eb602166823
Gift <giftgabanakgosi@gmail.com>
2024-05-29 14:49:06 +0200

    update to the magic button component

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Spotlight } from './ui/Spotlight'
 import { TextGenerateEffect } from './ui/TextGenerateEffect'
 import MagicButton from './MagicButton'
+import { FaLocationArrow } from 'react-icons/fa'
 
 
 const Hero = () => {
@@ -25,11 +26,13 @@ const Hero = () => {
                 <TextGenerateEffect className="text-center text-[40px] md:text-5xl lg:text-6xl" words="Transforming Concepts into Seamless Experience"/>
 
                 <p className='text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl'>
-                    Some extra text to be placed here
+                    Hello! I&apos;m Gift, a ReactJS Developer
                 </p>
 
                 <a className='' href="#">
-                    <MagicButton/>
+                    <MagicButton title="Show my work" icon={<FaLocationArrow width={50} height={50}/>}
+                        position='right'
+                    />
                 </a>
 
             </div>
